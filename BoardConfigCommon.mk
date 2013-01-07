3d111e18208d11767ac831c31990f5425ad63745
Shareef Ali <shareefalis@cyanogenmod.org>
2013-01-07 05:40:56 -0500

    msm8960: enable neon vfp4 instructions
    Change-Id: I2655f387e0facf2fcdc9d4a52a1dddfc0122b895

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,8 +26,10 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 # Architecture
 TARGET_CPU_SMP := true
 
-# Flags
+# Flags for Krait CPU
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
