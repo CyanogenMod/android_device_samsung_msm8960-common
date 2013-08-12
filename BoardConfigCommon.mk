7349cc6a1d44d1c05e79d25b274c0981bfed6144
Steve Kondik <shade@chemlab.org>
2013-08-12 13:18:15 -0700

    msm8960: Use retire fence from MDP driver
    Change-Id: Ic5d3e7d4cee127fabbdaf6f7b1dd2395347a22d9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,3 +101,6 @@ BOARD_HAVE_NEW_QC_GPS := true
 
 # Use CAF media driver variant for 8960
 TARGET_QCOM_MEDIA_VARIANT := caf
+
+# Use retire fence from MDP driver
+TARGET_DISPLAY_USE_RETIRE_FENCE := true
