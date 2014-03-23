ff20e54ba9fc8afdf27a94ff6c12a9b6333ea696
Dan Pasanen <dan.pasanen@gmail.com>
2014-03-23 07:39:51 -0500

    camera: this is not just for d2 anymore
    Change-Id: Ic6411156cf919c6fcc825405de95017ceffd55f8

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -57,7 +57,7 @@ camera_module_t HAL_MODULE_INFO_SYM = {
          version_major: 1,
          version_minor: 0,
          id: CAMERA_HARDWARE_MODULE_ID,
-         name: "d2-common Camera Wrapper",
+         name: "Samsung msm8960 Camera Wrapper",
          author: "The CyanogenMod Project",
          methods: &camera_module_methods,
          dso: NULL, /* remove compilation warnings */
