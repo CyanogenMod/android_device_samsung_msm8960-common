5e4fe44be1e7537a22de54bcf4de1a6aa7f4d1f3
Dave Daynard <nardholio@gmail.com>
2014-04-17 22:17:18 -0400

    msm8960: fix samsung camcorder mode the right way
    A lot of these modes are inter-dependent. Don't mess with Samsung camera
    mode if it's already been set.
    
    There is also a problem with apps which use the FFC for video chat, that
    don't set the recording hint properly. Detect them from the special
    video frame rate used.
    
    Change-Id: I8f1fc611a2f100d0438447c25103fc44917c5d87

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -156,6 +156,7 @@ char * camera_fixup_setparams(struct camera_device * device, const char * settin
     const char* camMode = params.get(KEY_SAMSUNG_CAMERA_MODE);
 
     bool isVideo = !strcmp(params.get(android::CameraParameters::KEY_RECORDING_HINT), "true");
+    bool enableZSL = !strcmp(params.get(android::CameraParameters::KEY_ZSL), "on");
 
     // fix params here
     // No need to fix-up ISO_HJR, it is the same for userspace and the camera lib
@@ -198,11 +199,17 @@ char * camera_fixup_setparams(struct camera_device * device, const char * settin
 
 #ifdef SAMSUNG_CAMERA_MODE
     /* Samsung camcorder mode */
-    params.set(KEY_SAMSUNG_CAMERA_MODE, isVideo ? "1" : "0");
+    if (!strcmp(camMode, "-1")) {
+        if (!strcmp(params.get(android::CameraParameters::KEY_PREVIEW_FRAME_RATE), "15") && !isVideo) {
+            // Do nothing. Hangouts actually likes the mode to be -1.
+        } else {
+        params.set(KEY_SAMSUNG_CAMERA_MODE, isVideo ? "1" : "0");
+        }
+    }
 #endif
 #ifdef ENABLE_ZSL
     /* Only activate ZSL if requested by the app! */
-    if (!strcmp(params.get(android::CameraParameters::KEY_ZSL), "on")) {
+    if (enableZSL) {
         params.set(android::CameraParameters::KEY_CAMERA_MODE, "1");
 #ifdef MAGIC_ZSL_1508
         if (!isVideo) {
