f0abd11a9122a209b69c7f5b2b6cfd2ae95f2556
Arne Coucheron <arco68@gmail.com>
2014-07-06 01:40:19 +0200

    device: Use custom PowerHAL
    * Based on CM PowerHAL, but modified to work better for
       this device and the current governors we use.
    
    Change-Id: If2e7ccda70a4f34e5fda0de3488e2ce33c131748

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,3 +135,6 @@ BOARD_SEPOLICY_UNION += \
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_MAX_PARTITIONS := 28
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# Build our own PowerHAL
+TARGET_POWERHAL_VARIANT :=
