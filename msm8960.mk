158e9c75afa7a6152efb233b38c95f9f85cf1c74
Dan Pasanen <dan.pasanen@gmail.com>
2014-12-06 17:53:29 -0600

    msm8960: disable audio resampler
    Change-Id: I22a7b9669f7804c54f8756be16f7a1891bfba3ca

--- msm8960.mk
+++ msm8960.mk
@@ -57,7 +57,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    af.resampler.quality=255 \
     lpa.decode=true \
     qcom.hw.aac.encoder=true \
     ro.config.vc_call_vol_steps=7
