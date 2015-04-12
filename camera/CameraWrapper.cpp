b4415d91251c4fa3a2000045ea01c3aac6b77673
Dave Daynard <nardholio@gmail.com>
2015-04-12 18:57:18 -0400

    msm8960-common: Switch camera wrapper to CameraParameters2 class
    Change-Id: I8f319896cc6fd6ba9873a40047aaccee69e5a83c

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -32,7 +32,7 @@
 #include <hardware/hardware.h>
 #include <hardware/camera.h>
 #include <camera/Camera.h>
-#include <camera/CameraParameters.h>
+#include <camera/CameraParameters2.h>
 #include <dlfcn.h>
 
 #define BACK_CAMERA_ID 0
@@ -352,7 +352,7 @@ static int camera_set_parameters(struct camera_device *device,
     __android_log_write(ANDROID_LOG_VERBOSE, LOG_TAG, settings);
 #endif
 
-    CameraParameters params;
+    CameraParameters2 params;
     params.unflatten(String8(settings));
 
     const char* camMode = params.get(CameraParameters::KEY_SAMSUNG_CAMERA_MODE);
@@ -457,7 +457,7 @@ static char *camera_get_parameters(struct camera_device *device)
 
     wrapper_camera_device_t *wrapper = (wrapper_camera_device_t *)device;
 
-    CameraParameters params;
+    CameraParameters2 params;
     params.unflatten(String8(parameters));
 
     // fix params here
