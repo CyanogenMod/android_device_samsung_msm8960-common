8d951ebb4b67b5a2a77c551de22b5b9758e81fdf
Dave Daynard <nardholio@gmail.com>
2015-05-10 21:40:48 -0400

    msm8960-common: fix LTE doesn't come up on boot on AT&T
    Multi-sim changed the name of the preferred APN file that the ril blobs
    are looking for, leading to default APN not getting set, leading to
    proper APN for LTE not getting set.
    
    Also remove ril logging files on boot, we don't need them
    
    Change-Id: Ie73a479b6e292170a0c63823984be67245861956

--- rootdir/etc/init.led.sh
+++ rootdir/etc/init.led.sh
@@ -4,3 +4,7 @@
 if [ `cat /sys/class/sec/led/led_pattern` = "6" ]; then
     echo 0 > /sys/class/sec/led/led_pattern
 fi
+
+/system/bin/toolbox rm /data/RS*.log
+
+/system/bin/toolbox ln -s "/data/data/com.android.providers.telephony/shared_prefs/preferred-apn1.xml" "/data/data/com.android.providers.telephony/shared_prefs/preferred-apn.xml"
