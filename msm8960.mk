a9c5661ca7517422f3f4f39951131bfb5942e1f0
Dave Daynard <nardholio@gmail.com>
2015-07-29 22:57:19 -0400

    fix merge derp: actually use le codecs
    Change-Id: Ie04ecc26f456fb93172116d1e3d522e132bfd443

--- msm8960.mk
+++ msm8960.mk
@@ -50,7 +50,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video_le.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
 
 # System Properties
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
