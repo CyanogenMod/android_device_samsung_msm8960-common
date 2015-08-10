d04f5807e974d2423caee81672b286543abb574e
khusika <khusikadhamar@gmail.com>
2015-08-10 19:55:51 +0700

    msm8960: Remove deprecated egl.cfg
    see here: https://github.com/CyanogenMod/android_frameworks_native/commit/993814255fc248454368ed9fe34b4703a05eaf99
    
    Change-Id: Ib29fc8685df1de424a0b50733acb0d194b8c9e4f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,7 +57,6 @@ BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_HARDWARE_CLASS += device/samsung/msm8960-common/cmhw
 
 # Display
-BOARD_EGL_CFG := device/samsung/msm8960-common/configs/egl.cfg
 BOARD_USES_LEGACY_MMAP := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 TARGET_DISPLAY_INSECURE_MM_HEAP := true
