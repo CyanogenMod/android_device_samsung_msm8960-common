2bcbe769ac06ebf439534a297c41f9c741a11150
Arne Coucheron <arco68@gmail.com>
2015-10-29 03:02:20 +0100

    device: Enable MIDI support
    Change-Id: I7b21d612f9b1ddb128371acca9fbe3d0a9cb9c7d

--- msm8960.mk
+++ msm8960.mk
@@ -44,7 +44,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
 
 # Media
 PRODUCT_COPY_FILES += \
