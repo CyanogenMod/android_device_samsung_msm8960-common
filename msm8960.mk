240362b8b5fd6bfd441154d695c085672a653c5b
Steve Kondik <steve@cyngn.com>
2015-11-29 20:57:15 -0600

    msm8960: Drop all the janky proprietary stuff we don't even use
    Change-Id: I276ac6b6912ecbcf4df5cef5e79c1bf6efdd71d5

--- msm8960.mk
+++ msm8960.mk
@@ -157,7 +157,6 @@ PRODUCT_COPY_FILES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    libdashplayer \
     libOmxCore \
     libOmxVdec \
     libOmxVenc \
@@ -165,11 +164,7 @@ PRODUCT_PACKAGES += \
     libOmxAmrEnc \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
-    libstagefrighthw \
-    qcmediaplayer
-
-PRODUCT_BOOT_JARS += \
-    qcmediaplayer
+    libstagefrighthw
 
 # Power
 PRODUCT_PACKAGES += \
