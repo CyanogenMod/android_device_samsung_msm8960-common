b598c3eb353ca7e2ab19f1ac7be9ecc34d196f93
Dave Daynard <nardholio@gmail.com>
2016-02-07 08:22:18 -0500

    msm8960-common: nuke the last remnants of crda
    Change-Id: Ibf4bb91d03a93192b3ee3c90c6e317b1ba4f037d

--- msm8960.mk
+++ msm8960.mk
@@ -120,13 +120,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.msm8960
 
-# CRDA
-PRODUCT_PACKAGES += \
-    crda \
-    linville.key.pub.pem \
-    regdbdump \
-    regulatory.bin
-
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8960 \
