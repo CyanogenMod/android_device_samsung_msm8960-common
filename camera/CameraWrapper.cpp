5eea57f14e790b391406aad48ceaab8fe3ab0ed9
Matrix Zone <matrixzonee@gmail.com>
2016-05-27 09:52:47 -0700

    msm8960: CameraWrapper: force 960x720 preview size
    Change-Id: I9b863a5c49ea542cd17270ff8a43591de72d245a

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -390,6 +390,9 @@ static int camera_set_parameters(struct camera_device *device,
         }
     }
 
+    if (id == FRONT_CAMERA_ID)
+       params.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES, "960x720");
+
     /* Are we in continuous focus mode? */
     if (strcmp(params.get(CameraParameters::KEY_FOCUS_MODE), "infinity") &&
        strcmp(params.get(CameraParameters::KEY_FOCUS_MODE), "fixed") && (id == BACK_CAMERA_ID)) {
